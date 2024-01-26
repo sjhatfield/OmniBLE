8f78c34440ad64579e4dd6c44245e2e23e4ee927
Joe Moran <mojo@moranfoundation.org>
2024-01-25 19:52:53 -0800

    Fix DASH pod deactivation for simulator by setting podComms delegate (#111)

--- OmniBLE/PumpManager/OmniBLEPumpManager.swift
+++ OmniBLE/PumpManager/OmniBLEPumpManager.swift
@@ -767,6 +767,9 @@ extension OmniBLEPumpManager {
 
         self.podComms = PodComms(podState: podState, myId: state.controllerId, podId: state.podId)
 
+        self.podComms.delegate = self
+        self.podComms.messageLogger = self
+
         setState({ (state) in
             state.updatePodStateFromPodComms(podState)
             state.scheduledExpirationReminderOffset = state.defaultExpirationReminderOffset
